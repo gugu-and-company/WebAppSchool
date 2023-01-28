162092fd178e6792a30116a0811a08ef2113b8f2
megumu.sakaguchi <megumu.sakaguchi@gmail.com>
2023-01-28 13:24:57 +0900

    test

--- src/web/22ch.php
+++ src/web/22ch.php
@@ -3,78 +3,98 @@
 
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="style.css" type="text/css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+
 </head>
 
 <body>
-
   <?php
-  require_once('./Post.php');
+  require_once('Post.php');
 
-  $POSTS_FILE_NAME = "./posts.json";
+  // jsonファイルからPostクラスの配列を取得する
+  $posts = readPostsFromJson();
+  // print_r($posts);
+  // echo "<br/>";
+  // echo "<br/>";
 
-  $posts;
-  $message = "";
-  $error_message = "";
+  // 画面から入力した値を変数に入れる
+  $newpost = $_GET["newpost"];
 
-  if (file_exists($POSTS_FILE_NAME)) {
+  // 現在日時を取得
+  $newdate = new DateTime('now');
+  $newdate = $newdate->format('Y-m-d H:i');
 
-    // Postクラスの配列を取得する
-    $posts = readPostsFromJson($POSTS_FILE_NAME);
-
-    $message = "Read Posts from JSON file data.";
-  } else {
-
-    $error_message = "JSON file Not found.";
+  // 入力された投稿と現在日時でPostクラスを作る
+  $p = new Post($newdate, $newpost);
+  array_push($posts, $p);
+  print_r($posts);
+  echo "<br/>";
+  echo "<br/>";
+  $ary = array();
+  // print_r($ary);
+  foreach ($posts as $i) {
+    echo "-----";
+    print_r($i);
+    echo "-----";
+    echo "<br/>";
+    // 入力された投稿と現在日時を含めた連想配列を作る
+    $ary2 = array('date' => $i->getDatetime(), 'post' => $i->getPost());
+    array_push($ary, $ary2);
   }
 
-  ?>
+  // print_r($ary);
 
-  <? if ($error_message) : ?>
-    <h3 class='text-danger'><?= $error_message ?></h3>
-  <? else : ?>
-    <h3 class='text-success'><?= $message ?></h3>
-    <?php foreach ($posts as $post) : ?>
-      <div class='card'>
-        <div class='dttm'> <?= $post->getDatetime(); ?> </div>
-        <div class='post'> <?= $post->getPost(); ?> </div>
-      </div>
-    <?php endforeach; ?>
-  <? endif; ?>
+  // 画面から入力した値をdata.jsonを配列で書き込み
+  $newjson = json_encode($ary, JSON_UNESCAPED_UNICODE);
+  file_put_contents("data.json", $newjson);
 
-  <?php
+  // Postクラスのオブジェクトでは、json形式に変換できない
+  // $newjson = json_encode($posts);
+  // print_r($newjson);
 
-  function readPostsFromJson($path)
+  // それぞれの投稿を取り出す
+  foreach ($posts as $post2) {
+    // $post = new Post($post['date'], $post['post']);
+    // $post->createNewPost($newpost);
+    //Postクラスのget関数を使って表示
+    echo "<div class ='card'>";
+    echo "<div class ='dttm'>" .  $post2->getDatetime() . "</br></div>";
+    echo "<div class ='post'>" .  $post2->getPost() . "</br></div>";
+    echo "</div>";
+  }
+  // for ($i = 0; $i <= count($posts) - 1; $i++) {
+  //   echo "<div class ='card'>";
+  //   echo "<div class ='dttm'>" . $posts[$i]->dttm . "</br></div>";
+  //   echo "<div class ='post'>" . $posts[$i]->post . "</br></div>";
+  //   echo "</div>";
+  // }
+
+  // data.jsonを読み込んで配列化
+  function readPostsFromJson()
   {
-    // data read from json file
-    $data = file_get_contents($path);
-    // debug
-    // echo "---raw json--------------------<br>";
-    // print_r($data);
-    // echo "<br>";
-    // echo "-------------------------------<br>";
-
-    // decode a data
-    $json = json_decode($data); // Objectとしてdecode
-    // debug
-    // echo "---decode json-----------------<br>";
+    //data.jsonを開く
+    $data_file_name = "data.json";
+    $data = file_get_contents($data_file_name);
+    // data.jsonを配列で読み込み
+    $json = json_decode($data, true);
     // print_r($json);
-    // echo "<br>";
-    // echo "-------------------------------<br>";
 
-    $posts = array();
-    foreach ($json as $post) {
-      // echo $post->datetime . "<br>";
-      // echo $post->post . "<br>";
-      $post = new Post($post->datetime, $post->post);
-      array_push($posts, $post);
+    $arr = array();
+    foreach ($json as $j) {
+      $post = new Post($j['date'], $j['post']);
+      // print_r($post);
+      // echo "<br/>";
+      // echo "<br/>";
+      array_push($arr, $post);
     }
-
-    return $posts;
+    //  print_r($arr);
+    return $arr;
   }
 
   ?>
-
 </body>
 
 </html>
